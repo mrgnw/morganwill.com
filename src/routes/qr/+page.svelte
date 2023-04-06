69dc91ed1f23ccb844c6c0827654128447b0a35a
Morgan <2504532+mrgnw@users.noreply.github.com>
2023-04-06 20:16:22 -0300

    qrcode

---
+++ src/routes/qr/+page.svelte
@@ -0,0 +1,13 @@
+<title>QR: morg.info</title>
+<h1>morgan's info</h1>
+<img src="/morg.info.svg" alt="morg.info">
+<style>
+h1{
+  font-size: 2em;
+  margin: 0;
+  padding: 0;
+  /* let's select a classy, modern font family */
+  font-family: 'Open Sans', sans-serif;
+  font-weight:100
+}
+</style>
