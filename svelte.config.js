f86c44ddb02b6b5c3df8d090968c454fa4400677
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-01-11 14:30:27 -0500

    add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,34 @@
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: [".svelte", ...mdsvexConfig.extensions],
+
 	kit: {
 		alias: {
-			$components: './src/components',
+			$components: "./src/components",
 		},
-		adapter: adapter(
-			{
-				pages: 'build',
-				assets: 'build',
-				fallback: 'index.html',
-				precompress: true,
-				strict: true,
-				routes: {
-					include: ['/*'],
-					exclude: ['<all>']
-				}
-			}
-		),
+		adapter: adapter({
+			pages: "build",
+			assets: "build",
+			fallback: "index.html",
+			precompress: true,
+			strict: true,
+			routes: {
+				include: ["/*"],
+				exclude: ["<all>"],
+			},
+		}),
 	},
 
 	preprocess: [
 		preprocess({
 			postcss: true,
 		}),
+		mdsvex(mdsvexConfig),
 	],
 };
 
