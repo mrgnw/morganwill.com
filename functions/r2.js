a370e10871d1528a4f3b942411420978b6be56b8
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-01-23 15:30:15 -0500

    assume .tsv

--- functions/r2.js
+++ functions/r2.js
@@ -1,7 +1,7 @@
 export async function onRequest(context, request) {
-	r2_path = request.path;
+	const r2_path = request.path.slice(3);
 	console.log(r2_path);
-  const obj = await context.env.R2_BUCKET.get(r2_path);
+  const obj = await context.env.R2_BUCKET.get(`${r2_path}.tsv`);
   if (obj === null) {
     return new Response('Not found', { status: 404 });
   }
