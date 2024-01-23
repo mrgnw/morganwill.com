3f21c591ef2b4d496c70265c4a8ff13580e5988b
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-01-23 15:19:43 -0500

    Update +page.server.js

--- src/routes/1/[slug]/+page.server.js
+++ src/routes/1/[slug]/+page.server.js
@@ -11,20 +11,13 @@ var oneliners = [
 export async function load({ params }) {
 	const { slug } = params;
 	const topic = slug.toLowerCase();
-	
-	if (!oneliners.includes(topic)) {
-		throw error(404, {
-			message: 'Not found',
-			code: 'NOT_FOUND'
-		});
-	}
 
 	const url = `https://txt.xcc.es/${topic}.tsv`;
 
 	const response = await fetch(url);
 	const text = await response.text();
 	const data = tsvParse(text);
-	// data.forEach(d => {
+
 	return {
 		columns: data.columns,
 		data: data
