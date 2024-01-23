430db00a033ce3a837857801d50da7f6311b533f
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-01-23 15:32:02 -0500

    Update r2.js

--- functions/r2.js
+++ functions/r2.js
@@ -3,7 +3,7 @@ export async function onRequest(context, request) {
 	console.log(r2_path);
   const obj = await context.env.R2_BUCKET.get(`${r2_path}.tsv`);
   if (obj === null) {
-    return new Response('Not found', { status: 404 });
+    return new Response('Not found', { status: 404, message: `${r2_path}.tsv` });
   }
   return new Response(obj.body);
 }
