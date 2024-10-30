ca452717ff7c3231c91901aeed10c0b1aa42d35c
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-10-30 22:04:01 +0100

    two countdowns

---
+++ src/routes/t90/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Countdown from '$components/Countdown.svelte';
+	let finalDate = new Date('2025-04-07T23:59:59');
+	let buffer_days = 90;
+	let ms_per_day = 24 * 60 * 60 * 1000;
+	let targetDate = finalDate.getTime() - (buffer_days * ms_per_day);
+
+	
+</script>
+
+<div class="flex flex-col gap-4 h-screen items-center justify-center">
+	<Countdown targetDate={targetDate}/>
+	<Countdown targetDate={finalDate} />
+</div>
