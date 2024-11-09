9b6e17a8aa8a06ce9007a8df27ee4c24b1a3b227
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-11-09 16:50:11 +0100

    Revert "sveltekit-sitemap"
    This reverts commit 64714294897119f27addcd2b292c01e2b9c5e52b.

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		Icons({
-			compiler: 'svelte',
-		})
-	]
+  plugins: [
+    sveltekit(),
+    Icons({
+      compiler: 'svelte',
+    })
+  ]
 })
