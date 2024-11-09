64714294897119f27addcd2b292c01e2b9c5e52b
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-11-09 16:48:14 +0100

    sveltekit-sitemap

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from "@sveltejs/kit";
 
-// export const prerender = true;
+export const prerender = true;
 
 export const GET: RequestHandler = async ({ url }) => {
 	const hostname = url.hostname;
