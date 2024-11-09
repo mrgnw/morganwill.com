f89520f24d15c6ae1dbebfb13e4dfeabad2d6ddd
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-11-09 16:01:31 +0100

    robots.txt should be .txt

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -1,14 +1,19 @@
-export async function GET({ url }) {
+import type { RequestHandler } from "@sveltejs/kit";
+
+export const GET: RequestHandler = ({ url }) => {
 	const hostname = url.hostname;
 	const sitemapUrl = `https://${hostname}/sitemap.xml`;
-	const robots = `User-agent: *
-Allow: /
-
-Sitemap: ${sitemapUrl}`;
+	const robots = [
+		"User-agent: *",
+		"Allow: /",
+		"",
+		`Sitemap: ${sitemapUrl}`,
+	].join("\n");
 
 	return new Response(robots, {
 		headers: {
 			"Content-Type": "text/plain",
+			"Cache-Control": "public, max-age=3600",
 		},
 	});
-}
+};
