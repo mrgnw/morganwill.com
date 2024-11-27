ae970efc77c7fad2640492a8285c556d67070e69
Morgan <2504532+mrgnw@users.noreply.github.com>
2024-11-27 12:22:02 +0100

    smaller qr

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,41 +5,49 @@ const all_links = [
 		title: 'photos',
 		alias: 'photo',
 		url: 'https://500px.com/p/morganw?view=licensing',
+		shortUrl: 'https://a.xcc.es/photos',
 		blurb: '500px photo portfolio',
 	},
 	{
 		title: 'instagram',
 		alias: 'ig',
 		url: 'https://instagram.com/zenfo.co',
+		shortUrl: 'https://a.xcc.es/ig',
 		blurb: 'Instagram profile',
 	},
 	{
 		title: 'LinkedIn',
 		alias: 'li',
 		url: 'https://linkedin.com/in/mrgnw',
+		shortUrl: 'https://a.xcc.es/li',
 		blurb: 'LinkedIn profile',
 	},
 	{
 		title: 'github',
 		alias: 'gh',
 		url: 'https://github.com/mrgnw',
+		shortUrl: 'https://a.xcc.es/gh',
 		blurb: 'GitHub profile',
 	},
 	{
 		title: 'bluesky',
+		alias: 'bsky',
 		url: 'https://bsky.app/profile/xcc.es',
+		shortUrl: 'https://a.xcc.es/bsky',
 		blurb: 'Bluesky profile',
 	},
 	{
 		title: 'message',
 		alias: 'dm',
 		url: 'https://t.me/mrgnw',
+		shortUrl: 'https://t.me/mrgnw',
 		blurb: 'Message on Telegram',
 	},
 	{
 		title: 'cv',
 		alias: 'cv',
 		url: 'https://cv.morganwill.com/',
+		shortUrl: 'https://cv.xcc.es/',
 		blurb: 'View my résumé/cv in HTML or download a PDF',
 	},
 ]
@@ -47,7 +55,8 @@ const all_links = [
 export async function load() {
 	const links = await Promise.all(
 		all_links.map(async (link) => {
-			const qr = await QRCode.toString(link.url, {
+			const url = link.shortUrl ?? link.url;
+			const qr = await QRCode.toString(url, {
 				type: "svg",
 				width: 164,
 			}
