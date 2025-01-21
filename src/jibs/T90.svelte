9da1c8f6a53be9468d9cfdef4da5e6a31bf036c7
Morgan <2504532+mrgnw@users.noreply.github.com>
2025-01-21 01:44:39 +0100

    t90 is just a countdown now

--- src/jibs/T90.svelte
+++ src/jibs/T90.svelte
@@ -5,12 +5,9 @@
 	let finalDate = new Date('2025-04-07T23:59:59');
 	let buffer_days = 90;
 	let ms_per_day = 24 * 60 * 60 * 1000;
-	let targetDate = finalDate.getTime() - (buffer_days * ms_per_day);
+	let targetDate = finalDate;
 </script>
 
 <div class="flex h-screen items-center justify-center">
-	<Countdowns countdowns={[
-		{ date: targetDate },
-		{ date: finalDate }
-	]} />
+	<Countdown targetDate={targetDate} />
 </div>
