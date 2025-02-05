49c5f7caeb01c3acc57dda0bbb7dd5fcf2db6395
Morgan <2504532+mrgnw@users.noreply.github.com>
2025-02-05 01:05:31 +0100

    copy identifier

--- src/routes/icons/+page.svelte
+++ src/routes/icons/+page.svelte
@@ -2,16 +2,24 @@
     /** @type {import('./$types').PageData} */
     export let data;
     import Icon from '@iconify/svelte';
-    let size = '4em'
+    import { toast, Toaster } from 'svelte-sonner';
+    let size = '4em';
+
+    async function copyToClipboard(text) {
+        await navigator.clipboard.writeText(text);
+        toast(`Copied "${text}"`);
+    }
 </script>
 
+<Toaster />
 <div class="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 gap-6 p-8">
     {#each data.icons as {id}}
         <button 
             class="w-10 h-10 flex items-center justify-center rounded-lg hover:bg-gray-100 transition-colors relative group"
             title={id}
+            on:click={() => copyToClipboard(id)}
         >
-            <Icon icon={id} width="{size}" height="{size}" />
+            <Icon icon={id} width={size} height={size} />
             <span class="opacity-0 group-hover:opacity-100 absolute -bottom-6 left-1/2 -translate-x-1/2 text-xs text-gray-600 whitespace-nowrap transition-opacity">
                 {id}
             </span>
