ea0e2eb34be830b50b2217e97a3f8f4a02e5e28d
Morgan <2504532+mrgnw@users.noreply.github.com>
2025-02-05 01:00:55 +0100

    shows icons

--- src/routes/icons/+page.svelte
+++ src/routes/icons/+page.svelte
@@ -1,13 +1,14 @@
 <script>
     /** @type {import('./$types').PageData} */
     export let data;
+    import Icon from '@iconify/svelte';
 </script>
 
 <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
-    {#each data.icons as {id, prefix, name}}
+    {#each data.icons as {id}}
         <div class="flex flex-col items-center p-4 border rounded hover:bg-gray-50">
-            <div class="w-8 h-8">
-                <iconify-icon icon={id} />
+            <div class="w-8 h-8 flex items-center justify-center">
+                <Icon icon={id} width="24" />
             </div>
             <span class="mt-2 text-sm text-gray-600 text-center break-all">{id}</span>
         </div>
