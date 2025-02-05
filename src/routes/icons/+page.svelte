a7786ac718fb0ae2ec705eb95b8f08b6fcd8ba1c
Morgan <2504532+mrgnw@users.noreply.github.com>
2025-02-05 01:02:43 +0100

    Update +page.svelte

--- src/routes/icons/+page.svelte
+++ src/routes/icons/+page.svelte
@@ -2,6 +2,7 @@
     /** @type {import('./$types').PageData} */
     export let data;
     import Icon from '@iconify/svelte';
+    let size = '4em'
 </script>
 
 <div class="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 gap-6 p-8">
@@ -10,7 +11,7 @@
             class="w-10 h-10 flex items-center justify-center rounded-lg hover:bg-gray-100 transition-colors relative group"
             title={id}
         >
-            <Icon icon={id} width="24" height="24" />
+            <Icon icon={id} width="{size}" height="{size}" />
             <span class="opacity-0 group-hover:opacity-100 absolute -bottom-6 left-1/2 -translate-x-1/2 text-xs text-gray-600 whitespace-nowrap transition-opacity">
                 {id}
             </span>
